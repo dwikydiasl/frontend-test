d1a9b3eefce7211eafe3531a8af72d672dd5ea9b
dwiky dias <diaslaksmana@gmail.com>
2020-07-11 10:33:18 +0700

    upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function closeTips() {
+  var x = document.getElementById("tips");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Header scroll
+
+$(function() {
+  var header = $(".header");
+   
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      header.addClass("scrolled");
+    } 
+    else {
+      header.removeClass("scrolled");
+    }
+  });
+  
+});
